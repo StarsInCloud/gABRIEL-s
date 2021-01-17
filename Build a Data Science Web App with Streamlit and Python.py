533c26d7ca31d9ce485aa6e90526f3cd42e97ffc
StarsInCloud <110220336+StarsInCloud@users.noreply.github.com>
2021-01-17 06:21:37 +0000

    updata readme

--- Build a Data Science Web App with Streamlit and Python.py
+++ Build a Data Science Web App with Streamlit and Python.py
@@ -10,4 +10,4 @@ DATA_URL = (
 )
 
 st.title("Motor Vehicle Collisions in New York City")
-st.markdown("This application is a Streamlit dashboard th
+st.markdown("This application is a Streamlit dashboard that 
