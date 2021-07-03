590cc063baf1fe693fb199a9b659e38944c1242d
StarsInCloud <110220336+StarsInCloud@users.noreply.github.com>
2021-07-03 17:16:07 +0000

    Update exec-committee.csv

--- Build a Data Science Web App with Streamlit and Python.py
+++ Build a Data Science Web App with Streamlit and Python.py
@@ -29,4 +29,4 @@ data[['latitude','longitude']].to_csv('lat_long.csv', index=False)
 
 
 st.header("Where are the most people injured in NYC?")
-injured_people = st.slider("Number of person
+injured_people = st.slider("Number of persons inj
