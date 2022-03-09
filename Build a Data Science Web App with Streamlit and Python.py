4dea4fd0deeab9d600f44a103e3e52442d3cc0e3
StarsInCloud <110220336+StarsInCloud@users.noreply.github.com>
2022-03-09 15:42:00 +0000

    timeconst.pl: Eliminate Perl warning

--- Build a Data Science Web App with Streamlit and Python.py
+++ Build a Data Science Web App with Streamlit and Python.py
@@ -62,4 +62,5 @@ st.write(pdk.Deck(
     ],
 ))
 if st.checkbox("Show raw data", False):
-    st.subheader("Raw data by minute between %i:00 and %i:00" % (hour, (hour + 1)
+    st.subheader("Raw data by minute between %i:00 and %i:00" % (hour, (hour + 1) % 24))
+ 
