6195de152ca53a5b6cefb4d2891529d92afad59e
StarsInCloud <110220336+StarsInCloud@users.noreply.github.com>
2022-06-01 18:07:13 +0000

    Update 2000-01-01-intro.md

--- Build a Data Science Web App with Streamlit and Python.py
+++ Build a Data Science Web App with Streamlit and Python.py
@@ -67,4 +67,6 @@ if st.checkbox("Show raw data", False):
 
 st.subheader("Breakdown by minute between %i:00 and %i:00" % (hour, (hour + 1) % 24))
 filtered = data[
-    (data[DATE_TIME].dt.hour >= hour) & (data[DATE_TIME].dt.hour < (hour +
+    (data[DATE_TIME].dt.hour >= hour) & (data[DATE_TIME].dt.hour < (hour + 1))
+]
+h
