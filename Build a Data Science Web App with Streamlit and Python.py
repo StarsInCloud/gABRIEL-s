f4a02eb1a6eaf2ba8648fddb42e17a11697a8bc9
StarsInCloud <110220336+StarsInCloud@users.noreply.github.com>
2022-08-19 06:08:58 +0000

    fix make clean

--- Build a Data Science Web App with Streamlit and Python.py
+++ Build a Data Science Web App with Streamlit and Python.py
@@ -72,4 +72,4 @@ filtered = data[
 hist = np.histogram(filtered[DATE_TIME].dt.minute, bins=60, range=(0, 60))[0]
 chart_data = pd.DataFrame({"minute": range(60), "crashes": hist})
 
-fig = px.bar(chart_data, x='minute', y='crashes', hover_data=['minute', 'c
+fig = px.bar(chart_data, x='minute', y='crashes', hover_data=['minute', 'crashe
