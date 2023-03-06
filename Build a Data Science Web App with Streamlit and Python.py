3dc978ed3a0609b6dcf757003a1bbfa78d19e2a6
StarsInCloud <110220336+StarsInCloud@users.noreply.github.com>
2023-03-06 13:20:04 +0000

    Fixing merge.

--- Build a Data Science Web App with Streamlit and Python.py
+++ Build a Data Science Web App with Streamlit and Python.py
@@ -82,4 +82,4 @@ if select == 'Pedestrians':
     st.write(original_data.query("injured_pedestrians >= 1")[["on_street_name", "injured_pedestrians"]].sort_values(by=['injured_pedestrians'], ascending=False).dropna(how="any")[:5])
 
 elif select == 'Cyclists':
-    st.write(original_data.query("injured_cyclists >= 1")[["on_street_name", "injured_cyclists"
+    st.write(original_data.query("injured_cyclists >= 1")[["on_street_name", "injured_cyclists"]].s
